fc6359641a667d2117c0e9877498f7e9288b76fb
emrahc4 <155392977+emrahc4@users.noreply.github.com>
2025-03-02 13:38:55 +0300

    Movie-App
    A modern movie application built with React.js and Redux Toolkit, featuring dynamic search, filtering, and state management for an enhanced user experience

---
+++ movie-app-main/movie-app-main/src/components/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './navbar.css'
+import { HOME, MY_LIST} from '../../constants/path'
+
+import { TiHome } from 'react-icons/ti'
+import { FaHeart } from 'react-icons/fa'
+import { PiFilmReelFill } from 'react-icons/pi'
+
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const Navbar = () => {
+    const { movies } = useSelector((store) => store.favorite)
+  return (
+    <div className='navbar'>
+        <div className="left">
+            <Link to={`${HOME}`}><h1>Movie App</h1></Link>  
+        </div>
+        <div className="center">
+            <Link to={`${HOME}`}><PiFilmReelFill className='logo'/></Link>
+        </div>
+        <div className="right">
+            <ul>
+                <li>
+                    <Link to={`${HOME}`}><TiHome /></Link>
+                </li>
+                <li>
+                    <Link to={`${MY_LIST}`}>
+                        <FaHeart />
+                        <div className='favorite-count'>{movies.length}</div>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
